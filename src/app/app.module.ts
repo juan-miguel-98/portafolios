eb6c42ea4687e7f790f29d01b0b39726d4e36219
Juan-Miguel-98 <juanmiguelgc1998@gmail.com>
2024-07-26 17:40:08 +0200

    ajustes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CuerpoComponent } from './componentes/cuerpo/cuerpo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CuerpoComponent  
   ],
   imports: [
     BrowserModule,
