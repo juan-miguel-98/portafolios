c535346ed26fec0e658924d3845d8865b7e64f6d
Juan-Miguel-98 <juanmiguelgc1998@gmail.com>
2024-08-31 19:33:25 +0200

    copiado de correo

--- src/app/pagina/contacto/contacto.component.ts
+++ src/app/pagina/contacto/contacto.component.ts
@@ -7,9 +7,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactoComponent implements OnInit {
 
+  correo = "juanmiguelgc1998@gmail.com";
+  copiado = false;
+  
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  copiar(){
+    this.copiado = true;
+    this.copiarContenido();
+  }
+
+  async copiarContenido() {
+    try {
+      await navigator.clipboard.writeText(this.correo);
+    } catch (err) {
+      console.error('Error al copiar: ', err);
+    }
   }
 
 }
